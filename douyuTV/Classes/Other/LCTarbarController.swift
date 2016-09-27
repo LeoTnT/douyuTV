12eb0bae69399788584c86e38c5af842c3493c95
lichao <lichao@lichaodemacbook-pro.local>
2016-09-27 11:26:07 +0800

    app图标以及基本框架搭建

---
+++ douyuTV/Classes/Other/LCTarbarController.swift
@@ -0,0 +1,20 @@
+//
+//  LCTarbarController.swift
+//  douyuTV
+//
+//  Created by lichao on 16/9/26.
+//  Copyright © 2016年 lichao. All rights reserved.
+//
+
+import UIKit
+
+class LCTarbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}
